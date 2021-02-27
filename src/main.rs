4e85c107e3a704dd9122ab8ba4dc9e9f1c0278db
Shafik Amin <shafik23@gmail.com>
2021-02-27 01:59:16 -0700

    Added error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing arguments: {}", err);
+        process::exit(1);
+    });
+}
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    let data = fs::read_to_string(filename).expect("File does not exist!");
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments to Config::new()");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
